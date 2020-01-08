917bd80fd51949fa880cdbf444d38dda1edc00e4
cano <mahorowa3088@gmail.com>
2020-01-08 14:00:34 +0900

    v1.1.10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pokemon-ierukana/cache@v1.1.9";
+const CACHE_NAME = "pokemon-ierukana/cache@v1.1.10";
 const FILES_TO_CACHE = ["/pokemon-ierukana/", "/pokemon-ierukana/index.html", "/pokemon-ierukana/index.js", "/pokemon-ierukana/index.css"];
 
 self.addEventListener("install", event => {
