597f738f5d324d06f1f2cdf4b35405f60879e636
cano <mahorowa3088@gmail.com>
2020-01-16 16:54:05 +0900

    ⚙️ service worker 生成用スクリプト

---
+++ scripts/workbox/generatesw.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const { generateSW } = require("workbox-build");
+
+generateSW({
+  swDest: path.resolve("./docs/sw.js"),
+  globDirectory: path.resolve("./docs"),
+  globPatterns: ["**/*.{html,css,js}"]
+}).then(({ count, size }) => {
+  console.log(`Generated sw.js, which will precache ${count} files, totaling ${size} bytes.`);
+});
