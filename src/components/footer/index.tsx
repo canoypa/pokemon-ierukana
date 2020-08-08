56ed0d0e76ead589c3c8abdbd8438e2ab16d46d5
Cano <contact.sasasaky@gmail.com>
2020-08-08 20:48:24 +0900

    footer を Footer に

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "preact";
+
+export const Footer: FC = () => (
+  <div>
+    <div>
+      {/* <time id="time">nn</time> */}
+      <span>Loading...</span>
+    </div>
+    <AnswerArea />
+  </div>
+);
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,6 +1,7 @@
 import { h, Fragment, FC } from "preact";
 import { Header } from "../../components/header";
 import { Main } from "../../components/main";
+import { Footer } from "../../components/footer";
 
 export const App: FC = () => (
   <Fragment>
