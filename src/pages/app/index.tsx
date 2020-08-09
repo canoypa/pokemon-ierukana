3852f29f833b2d37e85a322326e95060059fdf59
Cano <contact.sasasaky@gmail.com>
2020-08-09 12:38:31 +0900

    Provider

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,15 +1,17 @@
-import { h, Fragment, FC } from "preact";
+import { h, FC } from "preact";
 import { Header } from "../../components/header";
 import { Main } from "../../components/main";
 import { Footer } from "../../components/footer";
 import { BannerArea } from "../../components/banner-area";
+import { Provider } from "../../lib/preact-redux";
+import { store } from "../../store";
 
 export const App: FC = () => (
-  <Fragment>
+  <Provider store={store}>
     <Header />
     <Main />
     <Footer />
 
     <BannerArea />
-  </Fragment>
+  </Provider>
 );
