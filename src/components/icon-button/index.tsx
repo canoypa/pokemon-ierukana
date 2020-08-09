2b3c5729f5df170d83feb65e87185aae44c9e5a2
Cano <contact.sasasaky@gmail.com>
2020-08-09 13:47:11 +0900

    styling icon button

--- src/components/icon-button/index.tsx
+++ src/components/icon-button/index.tsx
@@ -1,10 +1,11 @@
 import { h, FC } from "preact";
+import styles from "./styles.scss";
 
 type Props = {
   icon: FC;
 };
 export const IconButton: FC<Props> = ({ icon: Icon }) => (
-  <button>
+  <button className={styles.iconButton}>
     <Icon />
   </button>
 );
