efdc5dc4ef83b05612202144c7952e2e21cfb8b8
Cano <contact.sasasaky@gmail.com>
2020-08-09 12:38:36 +0900

    dev log

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,3 +4,8 @@ import { reducers } from "./reducers";
 export const store = createStore(reducers);
 
 export const dispatch = store.dispatch;
+
+// Dev
+store.subscribe(() => {
+  console.log("new store: ", store.getState());
+});
