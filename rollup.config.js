d7a82a4bcc9aa580ef5b7ef8e849e53b142356f1
Cano <contact.sasasaky@gmail.com>
2020-08-09 12:43:37 +0900

    add rollup-plugin-node-globals

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import nodeGlobal from "rollup-plugin-node-globals";
 import typescript from "rollup-plugin-typescript2";
 import babel from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss";
@@ -18,6 +19,8 @@ export default {
 
     nodeResolve(),
     commonjs(),
+
+    nodeGlobal(),
 
     postcss({
       modules: true,
