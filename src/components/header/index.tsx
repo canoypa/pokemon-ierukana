6276a2d1456befa6fe1e3f12e89363ec7d87b25b
Cano <contact.sasasaky@gmail.com>
2020-08-10 16:16:08 +0900

    open settings

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,23 @@
 import { h, FC } from "preact";
 import { IconButton } from "../icon-button";
 import { IconSettings } from "../icons";
+import { setIsSettingsOpen } from "../../actions/is-settings-open";
 
-export const Header: FC = () => (
-  <header>
-    <section className="start">
-      <span id="title">Pokémon Ierukana</span>
-    </section>
-    <section className="end">
-      <IconButton icon={IconSettings} area-label="設定" />
-    </section>
-  </header>
-);
+export const Header: FC = () => {
+  const openSettings = () => setIsSettingsOpen(true);
+
+  return (
+    <header>
+      <section className="start">
+        <span id="title">Pokémon Ierukana</span>
+      </section>
+      <section className="end">
+        <IconButton
+          icon={IconSettings}
+          area-label="設定"
+          onClick={openSettings}
+        />
+      </section>
+    </header>
+  );
+};
