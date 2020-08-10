802b878c88aa2bd6c6055dfea5008f27ce689925
Cano <contact.sasasaky@gmail.com>
2020-08-10 16:27:50 +0900

    イベント

--- src/components/answer-area/index.tsx
+++ src/components/answer-area/index.tsx
@@ -2,9 +2,28 @@ import { h, FC } from "preact";
 import { IconButton } from "../icon-button";
 import { IconMic } from "../icons";
 
-export const AnswerArea: FC = () => (
-  <div>
-    <input type="text" placeholder="解答欄" aria-label="解答欄" />
-    <IconButton icon={IconMic} aria-label="音声入力" />
-  </div>
-);
+export const AnswerArea: FC = () => {
+  const answerInStr = () => {
+    // Todo
+  };
+
+  const startVoiceInput = () => {
+    // Todo
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="解答欄"
+        aria-label="解答欄"
+        onChange={answerInStr}
+      />
+      <IconButton
+        icon={IconMic}
+        aria-label="音声入力"
+        onClick={startVoiceInput}
+      />
+    </div>
+  );
+};
