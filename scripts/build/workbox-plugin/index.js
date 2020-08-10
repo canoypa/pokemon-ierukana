03b0c89f54782809be80357a86b058c6254915f8
Cano <contact.sasasaky@gmail.com>
2020-08-10 14:59:08 +0900

    ログなし

--- scripts/build/workbox-plugin/index.js
+++ scripts/build/workbox-plugin/index.js
@@ -6,10 +6,6 @@ export default function workboxPlugin() {
     swDest: resolve("./docs/sw.js"),
     globDirectory: resolve("./docs"),
     globPatterns: ["**/*.{html,css,js}"],
-  }).then(({ count, size }) => {
-    console.log(
-      `Generated sw.js, which will precache ${count} files, totaling ${size} bytes.`
-    );
   });
 
   return {
