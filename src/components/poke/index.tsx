2eaa0d3099d6572600b0eabb4706436967d5b98d
Cano <contact.sasasaky@gmail.com>
2020-08-11 21:53:14 +0900

    回答取得

--- src/components/poke/index.tsx
+++ src/components/poke/index.tsx
@@ -1,20 +1,36 @@
 import { h, FC } from "preact";
+import { useSelector } from "../../lib/preact-redux";
 import styles from "./styles.scss";
+
+const ImgBone: FC = () => <div class={styles.imgbone}>？</div>;
 
 type Props = {
   id: number;
   name: string;
 };
-export const Poke: FC<Props> = ({ id }) => (
-  <div class={styles.root}>
-    <div class={styles.imgarea}>
-      <div class={styles.imgbone}>？</div>
-    </div>
-    <div class={styles.primary}>
-      <div>
-        No: <span>{id}</span>
+export const Poke: FC<Props> = ({ id, name }) => {
+  const isAnswered = useSelector((store) => store.answered.has(id));
+
+  const displayName = isAnswered ? name : "？？？？";
+  const displayImg = isAnswered ? (
+    <img
+      class={styles.img}
+      src={`./assets/images/pokemon/${id}.png`}
+      alt={name}
+    />
+  ) : (
+    <ImgBone />
+  );
+
+  return (
+    <div class={styles.root}>
+      <div class={styles.imgarea}>{displayImg}</div>
+      <div class={styles.primary}>
+        <div>
+          No: <span>{id}</span>
+        </div>
+        <div class={styles.name}>{displayName}</div>
       </div>
-      <div class={styles.name}>？？？？</div>
     </div>
-  </div>
-);
+  );
+};
