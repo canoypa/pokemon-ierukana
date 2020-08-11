db4b6080d6f4a70678289652eca250487c6026a5
Cano <contact.sasasaky@gmail.com>
2020-08-11 21:57:03 +0900

    表示

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,4 +1,12 @@
 import { h, FC } from "preact";
+import { pokedex } from "../../pokedex";
+import { Poke } from "../poke";
 import styles from "./styles.scss";
 
-export const Main: FC = () => <div class={styles.root}></div>;
+export const Main: FC = () => (
+  <div class={styles.root}>
+    {pokedex.map(({ id, name }) => (
+      <Poke id={id} name={name} />
+    ))}
+  </div>
+);
