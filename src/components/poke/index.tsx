63ef7d5823c928c4e54ccec16f30a3f8dd969bbb
Cano <contact.sasasaky@gmail.com>
2020-08-11 21:43:18 +0900

    poke

--- src/components/poke/index.tsx
+++ src/components/poke/index.tsx
@@ -1,7 +1,20 @@
 import { h, FC } from "preact";
+import styles from "./styles.scss";
 
 type Props = {
   id: number;
   name: string;
 };
-export const Poke: FC<Props> = () => <div></div>;
+export const Poke: FC<Props> = ({ id }) => (
+  <div class={styles.root}>
+    <div class={styles.imgarea}>
+      <div class={styles.imgbone}>？</div>
+    </div>
+    <div class={styles.primary}>
+      <div>
+        No: <span>{id}</span>
+      </div>
+      <div class={styles.name}>？？？？</div>
+    </div>
+  </div>
+);
