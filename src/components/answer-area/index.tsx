67aae3414e70354dc5f13a20edcd8e572d2fe92a
Cano <contact.sasasaky@gmail.com>
2020-08-12 10:46:22 +0900

    音声入力中っぽいアニメーション

--- src/components/answer-area/index.tsx
+++ src/components/answer-area/index.tsx
@@ -4,12 +4,16 @@ import { IconMic } from "../icons";
 import { pokeKeywordToId } from "../../pokedex";
 import styles from "./styles.scss";
 import { setAnswered } from "../../actions/answered";
+import { useState } from "preact/hooks";
+import { classNames } from "../../utils/class-names";
 
 // Web Speech Api
 const { webkitSpeechRecognition, SpeechRecognition } = window;
 const ISpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
 
 export const AnswerArea: FC = () => {
+  const [inputtingVoice, setInputtingVoice] = useState(false);
+
   const answerInStr: JSX.GenericEventHandler<HTMLInputElement> = (event) => {
     const keyword = event.currentTarget.value;
     const id = pokeKeywordToId[keyword];
@@ -19,6 +23,33 @@ export const AnswerArea: FC = () => {
 
   const startVoiceInput = () => {
     // Todo
+
+    setInputtingVoice(true);
+
+    const speech = new ISpeechRecognition();
+
+    speech.lang = "ja-JP"; // 言語
+    speech.interimResults = true; // 暫定結果を取得可能にする
+    speech.continuous = true; // 認識後も続行する
+
+    speech.addEventListener("result", (event: any) => {
+      const result = event.results[event.resultIndex];
+      const alt = result[0];
+      const text = alt.transcript;
+
+      console.log("==================================================");
+      console.log("Rrsult Event");
+      console.log("Event: ", event);
+      console.log("Text: ", text);
+      console.log("isFinal: ", result.isFinal);
+      console.log("==================================================");
+
+      if (result.isFinal) {
+        speech.stop();
+      }
+    });
+
+    speech.start();
   };
 
   return (
@@ -31,11 +62,19 @@ export const AnswerArea: FC = () => {
         onChange={answerInStr}
       />
       {ISpeechRecognition && (
-        <IconButton
-          icon={IconMic}
-          aria-label="音声入力"
-          onClick={startVoiceInput}
-        />
+        <div
+          className={classNames(styles.speechButtonWrapper, {
+            [styles.inputtingVoice]: inputtingVoice,
+          })}
+        >
+          <div className={styles.speechButtonRipple}></div>
+          <IconButton
+            className={styles.absolute}
+            icon={IconMic}
+            aria-label="音声入力"
+            onClick={startVoiceInput}
+          />
+        </div>
       )}
     </div>
   );
