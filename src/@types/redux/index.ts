391c98ce426a849ea8dd50d760964d5b394bb381
Cano <contact.sasasaky@gmail.com>
2020-08-13 10:25:03 +0900

    fix

--- src/@types/redux/index.ts
+++ src/@types/redux/index.ts
@@ -1,6 +1,7 @@
-export type Action<T extends string, S = undefined> = {
+type Data<D> = D extends NonNullable<D> ? { data: D } : { data?: D };
+
+export type Action<T extends string, S = null> = {
   type: T;
-  data: S;
   error?: Error;
   meta?: unknown;
-};
+} & Data<S>;
