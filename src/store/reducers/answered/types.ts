ec1b33f4b992c9bf3a4938668639882f08294f9a
Cano <contact.sasasaky@gmail.com>
2020-08-13 10:04:37 +0900

    ResetAnsweredAction

--- src/store/reducers/answered/types.ts
+++ src/store/reducers/answered/types.ts
@@ -1,6 +1,7 @@
 import { Action } from "../../../@types/redux";
 
 export type SetAnsweredAction = Action<"SetAnsweredAction", number[]>;
+export type ResetAnsweredAction = Action<"ResetAnsweredAction">;
 
 export type State = Set<number>;
 export type Actions = SetAnsweredAction;
