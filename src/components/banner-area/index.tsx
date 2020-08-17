b78e4486d78fed60b6e50e94ce7005ed72e11c4b
Cano <contact.sasasaky@gmail.com>
2020-08-17 10:37:15 +0900

    新規回答を取得

--- src/components/banner-area/index.tsx
+++ src/components/banner-area/index.tsx
@@ -1,3 +1,18 @@
 import { h, FC } from "preact";
+import { useSelector } from "../../lib/preact-redux";
+import { useState } from "preact/hooks";
 
-export const BannerArea: FC = () => <div>{/* WIP */}</div>;
+export const BannerArea: FC = () => {
+  const answered = useSelector((s) => s.answered);
+
+  const [acqLength, setAcqLength] = useState(answered.size);
+  const [newAnswered, setNewAnswered] = useState<number[][]>([]);
+
+  setNewAnswered([
+    ...newAnswered,
+    [...answered].filter((_id, i) => acqLength < i),
+  ]);
+  setAcqLength(answered.size);
+
+  return <div>{/* WIP */}</div>;
+};
