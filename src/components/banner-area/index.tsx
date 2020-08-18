ec0c60877b4086ad730f716e5de5e138d9d58dc8
Cano <contact.sasasaky@gmail.com>
2020-08-18 10:54:42 +0900

    banner を作成

--- src/components/banner-area/index.tsx
+++ src/components/banner-area/index.tsx
@@ -1,19 +1,30 @@
 import { h, FC, JSX } from "preact";
 import { useSelector } from "../../lib/preact-redux";
-import { useState } from "preact/hooks";
+import { useState, useEffect } from "preact/hooks";
+import { pokedex } from "../../pokedex";
+import { Banner } from "../banner";
+import { getPokeImgURL } from "../../utils/get-poke-img-url";
 
 export const BannerArea: FC = () => {
   const answered = useSelector((s) => s.answered);
 
   const [banners, setBanners] = useState<JSX.Element[]>([]);
   const [acqLength, setAcqLength] = useState(answered.size);
-  const [newAnswered, setNewAnswered] = useState<number[][]>([]);
 
-  setNewAnswered([
-    ...newAnswered,
-    [...answered].filter((_id, i) => acqLength < i),
-  ]);
-  setAcqLength(answered.size);
+  const createBanner = (label: string, img: string) => (
+    <Banner label={label} img={img} />
+  );
+
+  useEffect(() => {
+    const newAnswered = [...answered].filter((_id, i) => acqLength < i);
+    setAcqLength(answered.size);
+
+    const banner = pokedex
+      .filter((p) => newAnswered.includes(p.id))
+      .map((p) => createBanner(getPokeImgURL(p.id), p.name));
+
+    setBanners([...banners, ...banner]);
+  }, [acqLength]);
 
   return <div>{/* WIP */}</div>;
 };
