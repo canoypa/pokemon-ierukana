575914e45c3baa5fada9f5532d6bed88663ae638
Cano <contact.sasasaky@gmail.com>
2020-08-21 13:15:17 +0900

    windowing し表示速度を改善
    応じてスタイル修正

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -2,11 +2,37 @@ import { h, FC } from "preact";
 import { pokedex } from "../../pokedex";
 import { Poke } from "../poke";
 import styles from "./styles.scss";
+import { Windowing } from "../windowing";
 
-export const Main: FC = () => (
-  <div class={styles.root}>
-    {pokedex.map(({ id, name }) => (
-      <Poke id={id} name={name} />
-    ))}
-  </div>
-);
+export const Main: FC = () => {
+  const rowHeight = 96;
+  const rowWidth = 256;
+  const rowCount = Math.ceil(pokedex.length / 4);
+  const colCount = ~~(window.innerWidth / rowWidth);
+
+  const renderPoke = (rowIndex: number) => {
+    const fromIndex = rowIndex * colCount;
+    const toIndex = fromIndex + colCount + 1;
+
+    return (
+      <div className={styles.pokerow}>
+        {pokedex
+          .filter(({ id }) => fromIndex < id && id < toIndex)
+          .map(({ id, name }) => (
+            <Poke id={id} name={name} />
+          ))}
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.root}>
+      <Windowing
+        className={styles.pokes}
+        rowCount={rowCount}
+        rowHeight={rowHeight}
+        render={renderPoke}
+      />
+    </div>
+  );
+};
---
+++ src/components/windowing/index.tsx
@@ -0,0 +1,61 @@
+import { h, FC, JSX, cloneElement } from "preact";
+import { useState, useRef, useEffect } from "preact/hooks";
+import styles from "./styles.scss";
+
+type Props = {
+  rowCount: number;
+  rowHeight: number;
+
+  render: (rowIndex: number) => JSX.Element;
+} & JSX.HTMLAttributes<HTMLDivElement>;
+export const Windowing: FC<Props> = ({
+  rowCount,
+  rowHeight,
+  render,
+  className,
+}) => {
+  const container = useRef<HTMLDivElement>();
+
+  const [containerHeight] = useState<number>(rowHeight * rowCount);
+  const [contents, setContents] = useState<JSX.Element[]>([]);
+
+  const re = () => {
+    const scrollTop = container.current.scrollTop;
+    const fromIndex = Math.max(0, ~~(scrollTop / rowHeight) - 5);
+    const toIndex = Math.min(
+      rowCount,
+      fromIndex + ~~(window.innerHeight / rowHeight) + 5
+    );
+
+    const range = (from: number, to: number) => {
+      const result = [];
+      for (let i = from; i <= to; i++) result.push(i);
+      return result;
+    };
+
+    const createContent = (rowIndex: number) => {
+      const props = {
+        key: rowIndex,
+        style: {
+          position: "absolute",
+          top: rowIndex * rowHeight,
+        },
+      };
+      return cloneElement(render(rowIndex), props);
+    };
+
+    const newContents = range(fromIndex, toIndex).map((i) => createContent(i));
+
+    setContents(newContents);
+  };
+
+  useEffect(() => re(), []);
+
+  return (
+    <div onScroll={re} ref={container} className={styles.root}>
+      <div className={className} style={{ height: containerHeight }}>
+        {contents}
+      </div>
+    </div>
+  );
+};
